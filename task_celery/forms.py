4cb4b626f9e7e3f39ea84e4d9ef3c41467c423ef
Yulia <krot.y.v@gmail.com>
2021-09-19 19:32:22 +0300

    hw_12

--- task_celery/forms.py
+++ task_celery/forms.py
@@ -5,14 +5,14 @@ from django.utils import timezone
 
 
 class Reminder(forms.Form):
-    email = forms.EmailField(max_length=100)
-    message = forms.CharField(max_length=300)
-    time = forms.DateTimeField(initial=timezone.now())
+    email = forms.EmailField(required=True, max_length=100)
+    message = forms.CharField(required=True, max_length=300)
+    time = forms.DateTimeField(required=True, initial=timezone.now())
 
     def clean_eta(self):
         time = self.cleaned_data['time']
         if time < timezone.now():
-            raise forms.ValidationError(f'The date must be later than {timezone.now()}')
-        if time > timezone.now() + timedelta(days=2):
-            raise forms.ValidationError(f'The date mustn\'t be later than {timezone.now() + timedelta(days=2)}')
+            raise forms.ValidationError('Wrong time')
+        if time > (timezone.now() + timedelta(days=2)):
+            raise forms.ValidationError('Wrong time')
         return time
